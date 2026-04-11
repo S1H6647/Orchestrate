d6dc4f5cb009c7ddc452543c5a195f19bed33766
Sanjit Hyanju <sanjithyanju99@gmail.com>
2026-04-11 20:30:26 +0545

    build: project initilization

---
+++ src/test/java/com/project/orchestrate/OrchestrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.orchestrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestrateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
